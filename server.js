6a5615c185145ed7587d4f526a9b34b7297683ea
Bernard van Niekerk <bernard.van.niekerk@icloud.com>
2019-08-29 06:54:14 +0200

    Added player control

---
+++ server.js
@@ -0,0 +1,223 @@
+import fs from 'fs'
+import YAML from 'yaml';
+import http from 'http';
+import polka from 'polka';
+import WebSocket from 'ws';
+import nunjucks from 'nunjucks';
+import spotifyWebApi from './spotify-web-api';
+import cors from 'cors';
+import compress from 'compression';
+import storage from 'node-persist';
+
+const templating = new nunjucks.Environment(new nunjucks.FileSystemLoader('views'));
+const render = (res) => (err, out) => {
+  if (err) {
+    console.error(err);
+    res.writeHead(500);
+    return res.end();
+  }
+  res.end(out);
+};
+
+const log = (messages) => console.log(new Date().toISOString(), ...messages);
+
+const { PORT = 3000 } = process.env;
+
+let config;
+const configs = [ 'client_id', 'client_secret', 'scope', 'redirect_uri' ];
+
+try {
+  const file = fs.readFileSync('./config.yaml', 'utf8');
+  config = YAML.parse(file);
+} catch (err) {
+  config = {};
+}
+for (const name of configs) {
+  if (name.toUpperCase() in process.env) {
+    config[name] = process.env[name.toUpperCase()];
+    if (name === 'scope') {
+      config[name] = YAML.parse(`[${String(config[name]).replace(/^\[|\]$/g, '')}]`);
+    }
+  } else if (!(name in config)) {
+    throw new Error(`Config option "${name}" or environment variable "${name.toUpperCase()}" not defined specified.`);
+  }
+}
+for (const name of [ 'persist' ]) {
+  if (name.toUpperCase() in process.env) {
+    config[name] = process.env[name.toUpperCase()];
+  }
+}
+
+let { persist, ...rest } = config;
+config = rest;
+
+const spotify = spotifyWebApi(config);
+
+const sessions = {};
+const deleteSession = (id) => {
+  persist && storage.removeItem(id);
+  delete sessions[id];
+};
+
+const app = polka();
+const server = http.createServer(app.handler);
+const wss = new WebSocket.Server({ server });
+
+let clients = [];
+
+const send = (message, client = null) => {
+  message = JSON.stringify(message);
+  if (client && client.readyState === WebSocket.OPEN) {
+    client.send(message);
+  } else {
+    clients.filter(client => client.readyState === WebSocket.OPEN).forEach(client => client.send(message));
+  }
+};
+
+const updateClients = () => {
+  clients = [ ...wss.clients ].filter(client => client.readyState === WebSocket.OPEN);
+};
+
+let delay = 1000;
+let tries = 0;
+const interval = setInterval(() => {
+  if (!clients.length) {
+    return;
+  }
+  Object.entries(sessions).forEach(async ([ id, session ]) => {
+    delay = 1000;
+    try {
+      let { body, ...rest } = await spotify.api(session).player.playing();
+      tries > 0 && log([ { 'ok': true } ]);
+      tries = 0;
+      send({ id, ...body });
+    } catch (err) {
+      if (err.code === 429) {
+        console.log(err.message);
+        delay = err.value * 1000;
+      } else {
+        if (tries > 5) {
+          console.error(new Date().toISOString(), { id }, err);
+          delay = 60000;
+          return;
+        }
+        log([ { id }, err ]);
+        delay = 2000;
+        tries += 1;
+      }
+    }
+  });
+}, delay);
+
+wss.on('connection', async ws => {
+
+  updateClients();
+  log([ { event: 'client_connected' } ]);
+  ws.on('message', data => {
+    data = JSON.parse(data);
+    const { id, name, query, body, request_id } = data;
+    if (id && id in sessions) {
+      const parts = name.split('.');
+      if (parts[0] === 'player') {
+        log([ 'Player' ]);
+        spotify.api(sessions[id]).player[parts[1]]({ query, body })
+        .then(({ res, body, ...rest }) => {
+          send({ id, request_id, request: parts[1], ...body, ...rest }, ws);
+        });
+      }
+    }
+
+
+    console.log(data);
+  });
+});
+
+(async function () {
+  let states = [];
+  const login = (req, res) => {
+    let { header, state } = spotify.init();
+    states.push(state);
+    res.writeHead(303, header);
+    send({ event: 'init_authentication' });
+    res.end();
+  };
+
+  const refresh = (id) => {
+    log([{ id, event: 'refresh_trigger', in: sessions[id].expires_in }]);
+    sessions[id].timeout = setTimeout(() => {
+      spotify.auth.refresh(sessions[id].refresh_token).then(async ({ access_token, expires_in }) => {
+        send({ id, event: 'token_refresh'});
+        sessions[id] = {
+          ...sessions[id],
+          access_token,
+          expires_in,
+          timestamp: new Date().toISOString()
+        };
+        persist && await storage.setItem(id, sessions[id]);
+        refresh(id);
+      }).catch(err => {
+        deleteSession(id);
+        console.error(new Date().toISOString(), err);
+      });
+    }, (sessions[id].expires_in - 360) * 1000);
+  }
+
+  if (persist) {
+    await storage.init({ dir: persist });
+    storage.forEach(async function({ key, value }) {
+      log([ { id: key, event: 'persistence loaded' } ]);
+      sessions[key] = {
+        ...value,
+        expires_in: 0
+      };
+      refresh(key);
+    });
+  }
+
+  const redirect = ({ query: { code, state } = {}, ...req }, res) => {
+    if (! states.includes(state)) {
+      console.error(new Date().toISOString(), 'Auth state not matched.');
+      res.writeHead(403);
+      return res.end('Forbidden');
+    }
+    states = states.filter(item => item !== state);
+    spotify.auth.token(code)
+    .then(auth => {
+      spotify.api(auth).user()
+      .then(async ({ body: { display_name, uri: id } }) => {
+        sessions[id] = { ...auth, timestamp: new Date().toISOString() };
+        await storage.setItem(id, sessions[id]);
+        send({ id, event: 'authenticated' });
+        refresh(id);
+        res.writeHead(303, { Location: `/?b=${Date.now()}` });
+        res.end();
+      });
+    });
+  };
+
+  app
+    .use(cors({ origin: true }))
+    .get('/', async (req, res) => {
+      log([ { http: '/' } ]);
+
+      const promises = [];
+      const results = await Promise.all(
+        Object.keys(sessions).map(id => {
+          return { id, promise: spotify.api(sessions[id]).user() };
+        }).map(({ id, promise }) => promise.catch(e => {
+          console.error(e);
+          deleteSession(id);
+          return e;
+        }))
+      );
+      let values = results.filter(result => !(result instanceof Error));
+      values = values.map(({ body }) => body);
+      templating.render('index.html', { users: values }, render(res));
+    })
+    .get('/login', login)
+    .get('/redirect', redirect);
+})();
+
+server.listen(PORT, err => {
+  log([ { event: 'listening', port: PORT } ]);
+});
---
+++ spotify-web-api.js
@@ -0,0 +1,134 @@
+import fetch from 'node-fetch';
+import uuid4 from 'uuid/v4';
+
+
+class ClientError extends Error {
+  constructor(name, code, value = null) {
+    super(`${name} (${code})`);
+    Error.captureStackTrace(this, ClientError);
+    this.name = name;
+    this.code = code;
+    this.value = value;
+  }
+}
+
+const queryStringify = query => Object.entries(query).map(pair => pair.join('=')).join('&');
+
+const responseError = res => {
+  if (res.status > 299) {
+      throw new ClientError(...[
+        res.statusText,
+        res.status,
+        ...(
+          res.status === 429 ? [ parseInt(res.headers.get('Retry-After')) ] : []
+        )
+      ]);
+  }
+  return res;
+};
+
+const handleResponse = async res => {
+  if (res.status === 204) {
+    return { body: {}, no_content: true, res };
+  }
+  if (res.status === 202) {
+    return { body: {}, accepted: true, res };
+  }
+  try {
+    let body = await res.json();
+    return { body, res } ;
+  } catch (err) {
+    console.error(err);
+    return { body: {}, error: err.message, res };
+  }
+};
+
+const api = ({
+  client_id,
+  client_secret,
+  redirect_uri,
+  scope = []
+}) => {
+
+  const endpoint = 'https://api.spotify.com/v1';
+
+  const request = new Proxy({}, {
+    get: (obj, prop) => ({ body, path, query }, { access_token, access_type = 'Bearer'}) => {
+      let url = `${endpoint}/${path}`;
+      if (query) {
+        url = `${url}?${queryStringify(query)}`;
+      }
+      return fetch(url, {
+        method: prop.toUpperCase(),
+        headers: {
+          Authorization: `${access_type} ${access_token}`
+        },
+        ...(body && { body: JSON.stringify(body) })
+      })
+      .then(responseError)
+      .then(handleResponse);
+    }
+  });
+
+  const token = ({ code, refresh_token, redirect_uri, grant_type = 'authorization_code' }) => {
+    return fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${Buffer.from(`${client_id}:${client_secret}`).toString('base64')}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: queryStringify({
+        grant_type,
+        ...(grant_type === 'authorization_code' ? { code, redirect_uri } : { refresh_token } )
+      })
+    })
+    .then(responseError)
+    .then(res => res.json())
+  };
+
+  return {
+
+    init: ({ state } = {}) => {
+      state = state || uuid4();
+      const location = `https://accounts.spotify.com/authorize?${queryStringify({
+        response_type: 'code',
+        client_id,
+        scope: scope.join(' '),
+        redirect_uri,
+        state
+      })}`;
+
+      return {
+        uri: location,
+        header: { Location: location },
+        state
+      }
+    },
+    auth: {
+      token: (code) => token({ code, redirect_uri }),
+      refresh: (refresh_token) => token({ grant_type: 'refresh_token', refresh_token })
+    },
+    api: (auth) => ({
+      user: (user = null) => request.get({
+        path: user ? `users/${user}` : 'me'
+      }, auth),
+      player: {
+        play: ({ query, body } = {}) => request.put({ path: 'me/player/play', query, body }, auth),
+        pause: ({ query } = {}) => request.put({ path: 'me/player/pause', query, body: {} }, auth),
+        previous: ({ query } = {}) => request.post({ path: 'me/player/previous', query }, auth),
+        next: ({ query } = {}) => request.post({ path: 'me/player/next', query }, auth),
+        recent: ({ query } = {}) => request.get({ path: 'me/player/recently-played', query }, auth),
+        playing: ({ query } = {}) => request.get({ path: 'me/player/currently-playing', query }, auth),
+        shuffle: ({ query } = {}) => request.put({ path: 'me/player/shuffle', query, body: {} }, auth),
+        repeat: ({ query } = {}) => request.put({ path: 'me/player/repeat', query, body: {} }, auth),
+        volume: ({ query } = {}) => request.put({ path: 'me/player/volume', query, body: {} }, auth),
+        seek: ({ query } = {}) => request.put({ path: 'me/player/seek', query, body: {} }, auth),
+        transfer: ({ query } = {}) => request.put({ path: 'me/player', query, body: {} }, auth),
+        devices: () => request.get({ path: 'me/player/devices' }, auth),
+      }
+    })
+  }
+
+};
+
+export default api;
